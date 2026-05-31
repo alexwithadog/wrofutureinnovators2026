3c77e362c4c6cf4f929e068fe1ebf80c5408a831
Alex Zhu <88217513+alexwithadog@users.noreply.github.com>
2026-05-31 00:31:05 -0400

    Create pin15.py

---
+++ pin15.py
@@ -0,0 +1,16 @@
+import os
+os.environ['JETSON_MODEL_NAME'] = 'JETSON_ORIN_NANO'
+import Jetson.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(15, GPIO.OUT, initial=GPIO.HIGH)
+
+print("Pin 15 HIGH. Ctrl-C to exit.")
+try:
+    while True: time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
